7895a5beb0b9ce8c4d195457159b986004f9ca03
Rahul <rahulna1996@gmail.com>
2021-05-29 12:10:52 +0530

    content of Readme file has been changed

--- src/components/ColorContainer.jsx
+++ src/components/ColorContainer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ColorPalette from "./ColorPalette";
 
-
 const ColorContainer = (props) => {
-    return <div className="color-container">
-        {props.colors.map((color, index) => {
-            return (
-                <ColorPalette
-                    key={index}
-                    hexCode={color.colorCode}
-                    colorName={color.colorName}
-
-                />
-            )
-        })}
+  return (
+    <div className="color-container">
+      {props.colors.map((color, index) => {
+        return (
+          <ColorPalette
+            key={index}
+            hexCode={color.colorCode}
+            colorName={color.colorName}
+          />
+        );
+      })}
     </div>
-}
+  );
+};
 
 export default ColorContainer;
